a568125371883d0fad4a1b6375c00824bbca2ec7
Kjersti <kjkv@nmbu.no>
2020-04-01 11:18:41 +0200

    Added plotting in main block and minor changes to phi func

--- oppgave2.py
+++ oppgave2.py
@@ -4,13 +4,15 @@ __author__ = "Kjersti Rustad Kvisberg & Ida Lunde Naalsund"
 __email__ = "kjkv@nmbu.no, idna@nmbu.no"
 
 import numpy
+import pandas as pd
+import matplotlib.pyplot as plt
 
 # Defining global variables
 h_bar = 1.055E-34
 m = 9.109E-31
 
 
-def psi_value(pos, pos_0, E, sigma):
+def psi(pos, pos_0, E, sigma):
     """ Time-independent wave function for wave packet. """
     k = numpy.sqrt(2*m*E)/h_bar
     a = 1/(numpy.pi**(1/4)*numpy.sqrt(sigma))
@@ -35,16 +37,14 @@ def phi(pos, pos_0, delta_pos, E, sigma, x_start_step, V_0, prev_phi):
     if pos == 0 or pos == L:
         return 0
     elif 0 < pos < L:
-        return prev_phi + (1j*h_bar/2*m)*\
-            (psi_value(pos+delta_pos, pos_0, E, sigma) -
-                2*psi_value(pos, pos_0, E, sigma) +
-                psi_value(pos-delta_pos, pos_0, E, sigma))/(delta_pos)**2 + \
-            (V(pos, x_start_step, V_0)*
-             psi_value(pos, pos_0, E, sigma))/1j*h_bar
+        return prev_phi + (1j*h_bar/2*m) * \
+               (psi(pos + delta_pos, pos_0, E, sigma) -
+                2 * psi(pos, pos_0, E, sigma) +
+                psi(pos - delta_pos, pos_0, E, sigma)) / delta_pos ** 2 \
+               + (V(pos, x_start_step, V_0) *
+                  psi(pos, pos_0, E, sigma)) / (1j * h_bar)
     else:
         raise ValueError("Invalid position, must be between 0 and L.")
-
-
 
 
 if __name__ == '__main__':
@@ -54,29 +54,37 @@ if __name__ == '__main__':
     x_start_step = 100E-9
     x_0 = 50E-9
     L = 200E-9
-    #print(psi_value(L, x_0, E, sigma))
-    #print(V(125E-9, x_start_step, V_0))
-    #print(phi(100E-9, x_0, E, sigma, x_start_step, V_0, 0.00000000003))
-    prev_phi = 0
+
     delta_pos = 1.5E-10
     delta_t = 2.25E-19
+    end_time = 1E-18
 
-    end_time = 5E-18
-    x = 0
     x_values = []
+    t_values = []
     phi_values = []
+
+    x = 0
     while x < L:
-        t = 0
-        while t < end_time:
-            current_phi = (phi(x, x_0, delta_pos, E, sigma, x_start_step,
-                              V_0, prev_phi))
-            prev_phi = current_phi
-            phi_values.append(current_phi)
-
-            t += delta_t
-
+        x_values.append(x)
         x += delta_pos
+    t = 0
+    while t < end_time:
+        t_values.append(t)
+        t += delta_t
 
+    phi_values = numpy.zeros((len(t_values), len(x_values)))
+    prev_phi = 0
 
+    for n in range(0, len(x_values)):
+        for m in range(0, len(t_values)):
+            x = x_values[n]
+            current_phi = phi(
+                x, x_0, delta_pos, E, sigma, x_start_step, V_0, prev_phi)
+            prev_phi = current_phi
+            phi_values.itemset((m, n), current_phi)
 
+    phi_value_data = pd.DataFrame(phi_values, columns=x_values)
 
+    for i in range(len(t_values)):
+        plt.plot(phi_value_data.iloc[i])
+        plt.show()
