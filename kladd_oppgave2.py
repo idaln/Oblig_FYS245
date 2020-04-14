bbdd0ec17e196985ce6b86ebbba7327bfb2b2e43
idaln <idna@nmbu.no>
2020-04-14 13:51:41 +0200

    Added imports

--- kladd_oppgave2.py
+++ kladd_oppgave2.py
@@ -2,6 +2,10 @@
 
 __author__ = "Ida Lunde Naalsund & Kjersti Rustad Kvisberg"
 __email__ = "idna@nmbu.no, kjkv@nmbu.no"
+import math
+import numpy as np
+import matplotlib.pyplot as plt
+
 
 # Oppgave2
 sigma = 1 * 10 ** -8
